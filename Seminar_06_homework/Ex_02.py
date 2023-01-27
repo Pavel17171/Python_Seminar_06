63fdf0c3646762ceddc1edaf3d8debcc56513ae8
[1] <[1.asus.rfrf]>
2023-01-27 13:30:38 +0300

    Add folder Seminar_06_homework

---
+++ Seminar_06_homework/Ex_02.py
@@ -0,0 +1,28 @@
+﻿# Ex_2. list comprehension, zip, enumerate
+
+# Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+#     Пример:
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+# - [2, 3, 5, 6] => [12, 15]
+
+from random import randint
+
+# Рандомное создание и заполнение скиска
+list1 = [randint(1, 9) for i in range(randint(3, 10))]
+print(f"Исходный список: {list1}")
+
+# Количество пар для умножения
+length = (len(list1) // 2 + 1 * (len(list1) % 2))
+
+# Создание кортежей для вывода пар элементов на печать
+list2 = zip(list1[ : length], list1[ : length - 1 - (len(list1)) % 2: -1])
+list2 = list(list2)
+
+# Создание списка результата перемножения элементов списка
+list3 = []
+for i in range(length):
+    list3.append(list1[i] * list1[-(i + 1)])
+
+# Вывод на печать
+for i, count in enumerate(list2, start = 1):
+    print(f'Пара чисел № {i} => {count} => Произведение равно: {list3[i-1]}')
---
+++ Seminar_06_homework/Ex_04.py
@@ -0,0 +1,17 @@
+﻿# Ex_4. enumerate
+
+# Задайте натуральное число N. Напишите программу, 
+# которая составит список простых множителей числа N.
+
+number = int(input("Введите число: "))
+list1 = []
+for i in range(2, number + 1):
+    while number % i == 0:
+        list1.append(i)
+        number //= i
+    else:
+        i += 1
+print("Простые множители числа: ")
+
+for i, count in enumerate(list1, start = 1):
+    print(f'Множитель № {i} => {count}')
